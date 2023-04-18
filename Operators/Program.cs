f7256863bc0fe82191bd44b52925ff3760dc95b0
brock810 <130688727+brock810@users.noreply.github.com>
2023-04-18 18:44:42 -0500

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata;
+
+namespace OperatorExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+          {
+        
+      var a = 63;
+      var b = 32;
+      var quotient = a / b;
+      var remainder = a % b;
+
+      Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+      Console.WriteLine("You should enter the radius of your circle");
+      var userInput = Console.ReadLine(); 
+      
+      var radius = double.Parse(userInput);
+      
+
+      Console.WriteLine(AreaOfCircle(radius));
+        
+        }
+
+     public static double AreaOfCircle(double radius)
+    {
+            
+       var area = Math.PI * Math.Pow(radius, 2);
+       return area;
+
+        }
+
+    }
+
+}
